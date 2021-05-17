5aa78b0d89826f9c9c4bf61674188099a0a60749
maraganderw@gmail.com <maraganderw@gmail.com>
2021-05-17 12:08:10 +0300

    test

---
+++ Book.h
@@ -0,0 +1,90 @@
+
+
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+using namespace std;
+
+
+
+
+
+class Book
+{
+    public:
+        Book();
+
+
+  enum Genre{
+
+    fiction = 1,nonficion,periodical,biography,children
+
+    };
+
+
+
+        void set_ISBN(string ISBN);
+        void set_title(string title);
+        void set_author(string author);
+        void set_copyright_date(string copyright_date);
+        void set_checked_out(int checked_out);
+        bool operator == (Book b1);
+       bool operator !=(Book b1);
+
+        string get_ISBN()
+        {
+            return ISBN;
+        }
+
+        string get_title()
+        {
+
+            return title;
+        }
+
+        string get_author()
+        {
+            return author;
+        }
+
+        string get_copyright_date()
+        {
+
+            return copyright_date;
+        }
+
+
+        int get_checked_out()
+        {
+        return checked_out;
+        }
+
+
+        void set_Genre(int number);
+
+        Genre get_Genre(void)
+        {
+        return g1;
+        }
+
+
+    protected:
+
+    private:
+
+    string ISBN;
+    string title;
+    string author;
+    string copyright_date;
+    int checked_out = 0;
+    Genre g1 = fiction;
+
+};
+
+
--- New_Book.cpp
+++ New_Book.cpp
@@ -1,123 +1,293 @@
-#include "std_lib_facilities.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Book.h"
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
 
 
+ostream  &operator <<( ostream & os,Book & b1);
 
-int Permutation (int,int);
-int Combination (int ,int);
-int Factorial(int);
 
 int main()
 {
 
+int number = 0;
+Book b1,b2;
+string user_input,user_input_2;
+cout << "please input an ISBN number";
+cin >> user_input;
+
+b1.set_ISBN(user_input);
+
+cout << "please input an ISBN number";
+cin >> user_input_2;
+
+b2.set_ISBN(user_input_2);
 
 
-
-
-int user_number_1 = 0,user_number_2 = 0;
-int user_choice = -1;
-int final_result = -1;
-char error_input = '0';
-cout << "Please type 2 integer numbers\n";
-
-try{
-if(!(cin >> user_number_1 >> user_number_2))
-throw runtime_error("w");
-}
-
-catch(...)
-{
-
-cout <<"wrong input ... program is now terminating\n";
-return 0;
-}
-
-
-cout << "Please type '1' for Permutation and '2' for Combination \n";
-
-cin >> user_choice;
-
-
-
-
-
-
-
-if( user_choice == 1)
-{
-
-final_result = Permutation(user_number_1,user_number_2);
-cout << "The result for Permutation is " << final_result << "\n";
-
-
-}
-
-else if (user_choice == 2)
-{
-final_result = Combination(user_number_1,user_number_2);
-cout << "The result for Combination is " <<final_result <<"\n";
-
-}
+if(b1!=b2)
+cout << "they are not equals\n";
 
 else
+cout <<"they are equals\n";
+
+
+
+b1.set_author("eminem");
+b1.set_title("LP1");
+
+b2.set_author("Dr dre");
+b2.set_title("chronic");
+
+
+cout << "Please Give me a number between 1-5\n";
+cin >> number;
+
+if((number <1) || (number >5))
+
 {
-cout <<"Not correct input ... Programm is now terminating\n";
+cout <<"Wrong number\n";
+return 0;
+
+}
+
+b1.set_Genre(number);
+cout << b1.get_Genre();
+
+
 return 0;
 }
-return 0;
+
+bool Isbn_check(string ISBN);
+
+
+Book::Book()
+{
+
 
 }
 
-int Permutation (int a,int b)
 
+
+
+void Book::set_ISBN(string ISBN)
 {
 
-b = Factorial (a-b);
-try
+// must be in form of n-n-n-x
+
+string second_string;
+int check = 0;
+int counter =0;
+for(int i =0;i< ISBN.size();i++)
 {
-a = Factorial(a);
-}
-catch(runtime_error &e)
 {
-cout << "too big number for Factorial\n";
-exit(1);
-}
-
-return a/b;
-
-}
-
-int Combination (int a ,int b)
-{
-
-a = Permutation(a,b);
-try
-{
-
-b = Factorial(b);
+if(ISBN[i] == '-')
+counter ++;
 
 }
-catch(runtime_error &e)
-{
 
-cout <<"too big number for Factorial \n";
-exit(1);
 }
 
-return a/b;
+if(counter !=3)
+{
+cout<<"Not valid ISBN";
+return ;
 }
 
-int Factorial(int number)
-{
-if(number > 10)
-{
 
-throw runtime_error("To big number\n");
+bool corect = Isbn_check(ISBN);
+
+if(corect)
+Book::ISBN = ISBN;
+
+
 }
 
-int result = 1;
-for(int i =1; i<= number; ++i)
-result = result *i;
 
-return result ;
+ostream & operator <<(ostream & os, Book & b1)
+{
+
+ return os << b1.get_title() <<"\n" << b1.get_author() << "\n" <<b1.get_ISBN() <<"\n";
+
+ }
+
+
+
+
+
+
+void Book::set_title(string title)
+{
+Book::title = title;
+
+}
+
+void Book::set_author(string author)
+{
+Book::author = author;
+
+}
+
+void Book:: set_copyright_date(string copyright_date)
+{
+Book::copyright_date = copyright_date;
+
+}
+
+
+void Book:: set_checked_out(int checked_out)
+{
+Book::checked_out = checked_out;
+
+}
+
+
+bool Isbn_check(string ISBN)
+{
+
+
+
+int last_postiiton =0;
+int last_numbers =0;
+
+while(1)
+{
+
+
+string second_string ;
+for(int i =last_postiiton; i<ISBN.size();i++)
+{
+
+
+if(ISBN[i] == '-')
+{
+last_postiiton = i+1;
+break;
+
+}
+second_string.push_back(ISBN[i]);
+
+
+}
+
+if(second_string.size() == 0)
+{
+cout <<"Wrong ISBN";
+return false;
+}
+
+
+
+for(int i =0; i< second_string.size();i++)
+{
+
+cout <<second_string;
+
+if((second_string[i]>122) || (second_string[i] < 97 ))
+{
+
+cout <<" Wrong ISBN value";
+return false;
+}
+
+}
+
+
+last_numbers++;
+if(last_numbers ==3 )
+break;
+
+}
+
+string second_string;
+
+for(int i =last_postiiton; i < ISBN.size();i++)
+
+{
+
+second_string.push_back(ISBN[i]);
+
+}
+
+cout <<second_string;
+
+for(int i=0; i<second_string.size();i++)
+{
+
+if((second_string[i] > 57) || (second_string[i]< 48))
+{
+
+cout << "ISBN is not correct";
+return false;
+}
+
+}
+
+
+
+
+return true;
+}
+
+
+
+
+
+
+ bool Book:: operator ==(Book b1)
+ {
+
+if(b1.get_ISBN() == get_ISBN())
+return true;
+
+else
+return false;
+
+ }
+bool Book:: operator !=(Book b1)
+{
+
+if(b1.get_ISBN() != get_ISBN())
+return true;
+
+else
+return false;
+
+}
+
+
+void Book:: set_Genre(int number)
+{
+    switch(number)
+{
+    case 1 :
+    Book::g1 = fiction;
+    break;
+
+    case 2:
+    Book::g1 = nonficion;
+    break;
+
+    case 3:
+
+    Book::g1 = periodical;
+    break;
+
+    case 4:
+
+    Book::g1 = biography;
+    break;
+
+
+    case 5:
+
+      Book::g1 = children;
+    break;
+
+}
 
 }
